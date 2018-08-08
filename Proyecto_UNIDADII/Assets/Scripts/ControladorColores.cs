e4b2d5efaa6dff16f1fe3e440579956737c475cb
AlejoPiston <daoc1094@gmail.com>
2018-08-08 05:08:08 -0500

    Menu
    Listo

---
+++ Proyecto_UNIDADII/Assets/Scripts/ControladorColores.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class ControladorColores : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject btn;
+   
+    
+    // Use this for initialization
+    void Start () {
+        btn = GameObject.Find("Menu");
+        btn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour b)
+    {
+
+       SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+               
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour b)
+    {
+       
+        Debug.Log("Sin presionar");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
