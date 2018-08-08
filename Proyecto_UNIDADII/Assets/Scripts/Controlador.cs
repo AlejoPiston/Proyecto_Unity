914ea7eda59bedb69fade7b712ebcf4c2084525d
AlejoPiston <daoc1094@gmail.com>
2018-08-08 04:38:29 -0500

    direcciones
    Listo

---
+++ Proyecto_UNIDADII/Assets/Scripts/Controlador.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+
+
+public class Controlador : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject btn;
+    public GameObject btn2;
+    public Animation ani;
+
+    // Use this for initialization
+    void Start () {
+        btn = GameObject.Find("Derecha");
+        btn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        btn2 = GameObject.Find("Menu");
+        btn2.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        ani.GetComponent<Animation>();
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour b)
+    {
+        
+        Debug.Log("Presionado");
+        switch (b.VirtualButtonName)
+        {
+            case "Menu":
+                SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+                break;
+            case "Derecha":
+                ani.Play("wave");
+                break;
+               
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour b)
+    {
+        ani.Play("idle");
+        Debug.Log("Sin presionar");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		
+	}
+    
+}
--- Proyecto_UNIDADII/Assets/Scripts/ControladorNivel.cs
+++ Proyecto_UNIDADII/Assets/Scripts/ControladorNivel.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ControladorNivel : MonoBehaviour {
+   
 
 	// Use this for initialization
 	void Start () {
+
+        
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+   
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
